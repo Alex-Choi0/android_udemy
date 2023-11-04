c16602d376cfb7b72c51da459ffb9a55614a28f0
Alex <choijeaho86@gmail.com>
2023-11-04 09:47:33 +0900

    finish lesson

--- app/src/main/java/com/example/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.firstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    AppCompatButton button;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editTextName);
+        button = findViewById(R.id.buttonOk);
+        result = findViewById(R.id.textViewResult);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.setText(editText.getText().toString());
+            }
+        });
     }
 }
