4b79574e7cfcde8dd535637dc979bcb0d62da0bb
Alex <choijeaho86@gmail.com>
2023-11-04 16:33:00 +0900

    finish lesson

--- app/src/main/java/com/example/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -14,31 +14,13 @@ import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView listView;
-//    Array countries[];
 
-    String countries[];
-
-    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = findViewById(R.id.list);
-        countries = getResources().getStringArray(R.array.countries);
 
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String ele = parent.getItemAtPosition(position).toString();
-
-                Toast.makeText(getApplicationContext(),"You click " + ele, Toast.LENGTH_LONG).show();
-            }
-        });
 
     }
 }
